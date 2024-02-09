6afaa1079befcc7fa592c3a07670c7b37c03a517
Khalil Jemal Brewington <151947960+KjemalB14@users.noreply.github.com>
2024-02-09 10:07:36 -0500

    adding comments to code

--- Test.py
+++ Test.py
@@ -1,11 +1,15 @@
 from gpiozero import LED
 from time import sleep
 
+#set led input to pin 17
 led = LED(17)
 
+#setting the led up to turn off and on every second
 while True:
     led.on()
     sleep(1)
     led.off()
     sleep(1)
+
+
     
